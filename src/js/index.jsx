36ea16035e97fe42408d2aebfb465498dbc84109
hankfrank <henryfrank1@yahoo.com>
2020-04-08 11:16:11 -0700

    updates react, react-dom, webpack and webpack-cli versions

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers';
 import App from './app';
 
 /* eslint-disable no-underscore-dangle */
